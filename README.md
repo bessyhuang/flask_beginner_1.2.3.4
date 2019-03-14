6d9ea88e1f6e4227c19aa2462115652a03b9c75b
chia <404040523@gapp.fju.edu.tw>
2019-03-14 23:50:44 +0800

    練習：05資料庫-SQLAlchemy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# flask_beginner_1.2.3.4
+﻿# flask_beginner_1.2.3.4
 Flask 網頁開發 第二版 (練習：01安裝、02基本app結構、03模板、04 web 表單)
+
+# flask_beginner_5
+Flask 網頁開發 第二版 (練習：05資料庫-SQLAlchemy)
--- hello.py
+++ hello.py
@@ -1,34 +1,78 @@
 from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
-from flask import abort
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired
+from flask import session, redirect, url_for
+
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+from flask import flash
+from flask_moment import Moment
+from datetime import datetime
+
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'AEHCFOAHMMFCCJCPW'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db) 
 
-@app.route('/')
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique = True)
+	#user = db.relationship('User', backref='role')
+	users = db.relationship('User', backref='role', lazy='dynamic')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), unique = True, index = True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+	name = StringField('what is your name?', validators=[DataRequired()] )
+	submit = SubmitField('Submit')
+	
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '''<h1>Hello World!</h1><br>
-            <p>Your browser is {}</p>'''.format(user_agent)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+    	
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('webform.html', form=form, name=session.get('name'), current_time=datetime.utcnow(), known=session.get('known', False))
 
-@app.route('/cookie')
-def cookie():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer','42')
-    return response
-
-@app.route('/redirect') ##
-def redirect():
-    return redirect(url_for('/'))
-
-@app.route('/test/<id>') ##
-def test(id):
-    test = load_user(id)
-    if not test:
-        abort(404)
-    return '<h1>Hello, {}!</h1>'.format(test.name)
+@app.shell_context_processor
+def make_shell_context():
+	return dict(db=db, User=User, Role=Role)
