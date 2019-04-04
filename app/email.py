03c0c5640e0ae8a914e009e3cfee12d675c91028
bessyhuang <42068007+bessyhuang@users.noreply.github.com>
2019-04-04 17:09:45 +0800

    Update email.py

--- app/email.py
+++ app/email.py
@@ -6,8 +6,7 @@ from . import mail
 
 #send_email(收件人地址, 主旨, email內文模板, 關鍵字引數)
 def send_email(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
-
+    app = current_app._get_current_object()     ###
     msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject, sender = app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     #msg.html = render_template(template + '.html', **kwargs)
