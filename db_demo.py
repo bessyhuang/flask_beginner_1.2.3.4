f093420dec982955ec02688adf6760328ddd2510
bessyhuang <42068007+bessyhuang@users.noreply.github.com>
2019-04-04 17:11:21 +0800

    Update db_demo.py

--- db_demo.py
+++ db_demo.py
@@ -1,18 +1,23 @@
 ### mariadb 連接 db_demo.py 與 webform.html ###
+# 主腳本 #
 import os
 
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate #用Flask-Migrate進行資料庫遷移
 
+#建立一個App，從環境變數或預設來取得組態設置。
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+##初始化Flask-Migrate
 migrate = Migrate(app, db) #用Flask-Migrate進行資料庫遷移
 
+##Python殼層的自訂context
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-### test 單元測試啟動命令 ###
+### test "單元測試"啟動命令 ###
 @app.cli.command()
 def test():
     """Run the unit tests."""
